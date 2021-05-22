f119f96fbd5062da70a861bdc71b329f29ca7854
ryanascherr <ryanascherr@gmail.com>
2021-05-21 21:35:10 -0400

    Tweaked Mario jumpo animation to start on full page load.

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let delayInMilliseconds = 1500;
+$(document).ready(function() {
+    
+
+
+let delayInMilliseconds = 1700;
 
 setTimeout(function() {
     $(".slide-from-bottom").removeClass("transparent");
 }, delayInMilliseconds);
+
+
+});
+
+$(window).on('load', function () {
+    $("body").addClass("loaded");
+});
