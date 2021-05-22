8af8df0914a33cad5766ae88476b5a37506d49d0
ryanascherr <ryanascherr@gmail.com>
2021-05-22 09:58:01 -0400

    Further refined animation timing.

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-$(document).ready(function() {
-    
-
-
 let delayInMilliseconds = 1700;
-
-setTimeout(function() {
-    $(".slide-from-bottom").removeClass("transparent");
-}, delayInMilliseconds);
-
-
-});
 
 $(window).on('load', function () {
     $("body").addClass("loaded");
+    setTimeout(function() {
+        $(".slide-from-bottom").removeClass("transparent");
+    }, delayInMilliseconds);
 });
