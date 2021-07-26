727ac41fccc2a372693d4aa79e70a2c5d0711b0a
ryanascherr <ryanascherr@gmail.com>
2021-07-26 09:37:00 -0400

    Prevented event bubbling on repo and page links. Removed unnecessary code in js file.

--- script.js
+++ script.js
@@ -133,10 +133,6 @@ textSubmitButton.addEventListener("keydown", function(event) {
   }
 });
 
-// var input = document.getElementById("myInput");
-// input.addEventListener("keyup", function(event) {
-//     if (event.keyCode === 13) {
-//         event.preventDefault();
-//         document.getElementById("myBtn").click();
-//     }
-// });
+$(".button").click(function(event) {
+  event.stopPropagation();
+});
