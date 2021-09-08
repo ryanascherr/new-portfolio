2a51d183184460f7f6db9a03d48aa56c6ed1dd2b
ryanascherr <ryanascherr@gmail.com>
2021-09-08 15:57:57 -0400

    Cleaned up code.

--- script.js
+++ script.js
@@ -1,66 +1,82 @@
 let delayInMilliseconds1 = 1700;
 let delayInMilliseconds2 = 2200;
 let delayInMilliseconds3 = 2700;
-let longerDelayInMilliseconds = 1700;
 
 $(window).on('load', function () {
-    $("body").addClass("loaded");
-    setTimeout(function() {
-        $(".slide-from-bottom-one").removeClass("transparent");
-        $(".block-one").addClass("hidden");
-        $(".block-one-empty").removeClass("hidden");
-    }, delayInMilliseconds1);
-    setTimeout(function() {
-      $(".slide-from-bottom-two").removeClass("transparent");
-      $(".block-two").addClass("hidden");
-      $(".block-two-empty").removeClass("hidden");
+  $("body").addClass("loaded");
+  setTimeout(function () {
+    $(".slide-from-bottom-one").removeClass("transparent");
+    $(".block-one").addClass("hidden");
+    $(".block-one-empty").removeClass("hidden");
+  }, delayInMilliseconds1);
+  setTimeout(function () {
+    $(".slide-from-bottom-two").removeClass("transparent");
+    $(".block-two").addClass("hidden");
+    $(".block-two-empty").removeClass("hidden");
   }, delayInMilliseconds2);
-  setTimeout(function() {
+  setTimeout(function () {
     $(".slide-from-bottom-three").removeClass("transparent");
     $(".block-three").addClass("hidden");
     $(".block-three-empty").removeClass("hidden");
-}, delayInMilliseconds3);
+  }, delayInMilliseconds3);
 });
 
-$(window).on("load",function() {
-    $(window).scroll(function() {
-      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      $(".fade").each(function() {
-        /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
-        
-        /* If the element is completely within bounds of the window, fade it in */
-        if (objectBottom < windowBottom - 50) { //object comes into view (scrolling down)
-          if ($(this).css("opacity")==0) {$(this).fadeTo(50,1);}
-        }
-      });
-    }).scroll(); //invoke scroll-handler on page-load
-  });
+$(window).on("load", function () {
+  $(window).scroll(function () {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function () {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom - 50) { //object comes into view (scrolling down)
+        if ($(this).css("opacity") == 0) { $(this).fadeTo(50, 1); }
+      }
+    });
+  //invoke scroll-handler on page-load
+  }).scroll();
+});
 
-  $("#time").html(moment().format('h:mm'));
+$("#time").html(moment().format('h:mm'));
 
 let isLightMode = true;
 
-$(".mode").click(function() {
-    if (isLightMode) {
-        $(".icon").html("wb_sunny");
-        isLightMode = false;
-        $("body").removeClass("light-mode");
-        $("body").addClass("dark-mode");
-        $(".project").addClass("dark-mode-shadow");
-        return;
-    } 
-    if (isLightMode == false) {
-        $(".icon").html("brightness_2");
-        isLightMode = true;
-        $("body").removeClass("dark-mode");
-        $("body").addClass("light-mode");
-        $(".project").removeClass("dark-mode-shadow");
-        return;
-    }
+$(".mode").click(function () {
+  if (isLightMode) {
+    $(".icon").html("wb_sunny");
+    isLightMode = false;
+    $("body").removeClass("light-mode");
+    $("body").addClass("dark-mode");
+    $(".project").addClass("dark-mode-shadow");
+    return;
+  }
+  if (isLightMode == false) {
+    $(".icon").html("brightness_2");
+    isLightMode = true;
+    $("body").removeClass("dark-mode");
+    $("body").addClass("light-mode");
+    $(".project").removeClass("dark-mode-shadow");
+    return;
+  }
 })
 
-$(".project-one").click(function(){
+// $("#email-submit").click(function (event) {
+//   event.preventDefault();
+//   let sender = "katie@katie.com";
+//   Email.send({
+//     Host: "smtp.gmail.com",
+//     Username: "sender@email_address.com",
+//     Password: "Enter your password",
+//     To: 'ryanascherr@gmail.com',
+//     From: sender,
+//     Subject: "Message from Portfolio!",
+//     Body: "Well that was easy!!",
+//   })
+//     .then(function (message) {
+//       alert("mail sent successfully")
+//     });
+// })
+
+$(".project-one").click(function () {
   $(".one").slideToggle();
   $(".two").slideUp();
   $(".three").slideUp();
@@ -69,7 +85,7 @@ $(".project-one").click(function(){
   $(".six").slideUp();
 });
 
-$(".project-two").click(function(){
+$(".project-two").click(function () {
   $(".two").slideToggle();
   $(".one").slideUp();
   $(".three").slideUp();
@@ -78,7 +94,7 @@ $(".project-two").click(function(){
   $(".six").slideUp();
 });
 
-$(".project-three").click(function(){
+$(".project-three").click(function () {
   $(".three").slideToggle();
   $(".one").slideUp();
   $(".two").slideUp();
@@ -87,7 +103,7 @@ $(".project-three").click(function(){
   $(".six").slideUp();
 });
 
-$(".project-four").click(function(){
+$(".project-four").click(function () {
   $(".four").slideToggle();
   $(".one").slideUp();
   $(".two").slideUp();
@@ -96,7 +112,7 @@ $(".project-four").click(function(){
   $(".six").slideUp();
 });
 
-$(".project-five").click(function(){
+$(".project-five").click(function () {
   $(".five").slideToggle();
   $(".one").slideUp();
   $(".two").slideUp();
@@ -105,7 +121,7 @@ $(".project-five").click(function(){
   $(".six").slideUp();
 });
 
-$(".project-six").click(function(){
+$(".project-six").click(function () {
   $(".six").slideToggle();
   $(".one").slideUp();
   $(".two").slideUp();
@@ -114,7 +130,7 @@ $(".project-six").click(function(){
   $(".five").slideUp();
 });
 
-$(".text-button").click(function() {
+$(".text-button").click(function () {
   if ($(".text-box").val() == "") {
     return;
   }
@@ -124,30 +140,30 @@ $(".text-button").click(function() {
   let paragraph = $("<p></p>").text(text);
   let innerDiv = $("<div></div>").addClass("text-bubble-right");
   let outerDiv = $("<div></div>").addClass("text-box-right");
-
+  
   innerDiv.append(paragraph);
   outerDiv.append(innerDiv);
   $(".texting").append(outerDiv);
 })
 
 const textSubmitButton = document.querySelector(".text-box");
-textSubmitButton.addEventListener("keydown", function(event) {
+textSubmitButton.addEventListener("keydown", function (event) {
   if (event.keyCode === 13) {
-      if ($(".text-box").val() == "") {
-        return;
-      }
-      let text = $(".text-box").val().trim();
-      $(".text-box").val("");
-      let paragraph = $("<p></p>").text(text);
-      let innerDiv = $("<div></div>").addClass("text-bubble-right");
-      let outerDiv = $("<div></div>").addClass("text-box-right");
-    
-      innerDiv.append(paragraph);
-      outerDiv.append(innerDiv);
-      $(".texting").append(outerDiv);
+    if ($(".text-box").val() == "") {
+      return;
+    }
+    let text = $(".text-box").val().trim();
+    $(".text-box").val("");
+    let paragraph = $("<p></p>").text(text);
+    let innerDiv = $("<div></div>").addClass("text-bubble-right");
+    let outerDiv = $("<div></div>").addClass("text-box-right");
+
+    innerDiv.append(paragraph);
+    outerDiv.append(innerDiv);
+    $(".texting").append(outerDiv);
   }
 });
 
-$(".button").click(function(event) {
+$(".button").click(function (event) {
   event.stopPropagation();
 });
